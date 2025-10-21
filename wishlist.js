14bf2d802f4ec439c5f5777d8fc21472b4baa50d
LeBochuliak <lena.bochuliak@gmail.com>
2025-10-21 20:49:45 +0000

    Deploying to gh-pages from @ LeBochuliak/practice-js-3@3e709a5ca5a7b8f42336236360e123c47254756b 🚀

---
+++ wishlist.js
@@ -0,0 +1,2 @@
+import{c as a,w as o,p as t,a as n,m as r,b as c}from"./assets/handlers-Mz18Kr9x.js";import"./assets/vendor-uumJaN6-.js";a();i();async function i(){const s=JSON.parse(localStorage.getItem("wishlist"))||[];o(s)}t.addEventListener("click",n);r.addEventListener("click",s=>{c(s),s.target.classList.contains("modal-product__btn--wishlist")&&(t.innerHTML="",i())});
+//# sourceMappingURL=wishlist.js.map
